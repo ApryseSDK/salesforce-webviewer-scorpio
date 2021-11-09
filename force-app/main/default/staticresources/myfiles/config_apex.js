c6b2b62d990186aa99b539c4596f79778de5797e
Swoshua <jplore4@gmail.com>
2021-11-09 11:27:18 -0800

    update syntax to latest version

--- force-app/main/default/staticresources/myfiles/config_apex.js
+++ force-app/main/default/staticresources/myfiles/config_apex.js
@@ -31,15 +31,15 @@ window.CoreControls.setCustomFontURL('https://pdftron.s3.amazonaws.com/custom/ID
 let currentDocId;
 
 async function saveDocument() {
-  const doc = docViewer.getDocument();
+  const doc = instance.Core.documentViewer.getDocument();
   if (!doc) {
     return;
   }
-  readerControl.openElement('loadingModal');
+  instance.openElement('loadingModal');
 
   const fileType = doc.getType();
   const filename = doc.getFilename();
-  const xfdfString = await docViewer.getAnnotationManager().exportAnnotations();
+  const xfdfString = await instance.Core.documentViewer.getAnnotationManager().exportAnnotations();
   const data = await doc.getFileData({
     // Saves the document with annotations in it
     xfdfString
@@ -68,13 +68,13 @@ window.addEventListener('viewerLoaded', async function () {
    * On keydown of either the button combination Ctrl+S or Cmd+S, invoke the
    * saveDocument function
    */
-  readerControl.hotkeys.on('ctrl+s, command+s', e => {
+   instance.hotkeys.on('ctrl+s, command+s', e => {
     e.preventDefault();
     saveDocument();
   });
 
   // Create a button, with a disk icon, to invoke the saveDocument function
-  readerControl.setHeaderItems(function (header) {
+  instance.setHeaderItems(function (header) {
     var myCustomButton = {
       type: 'actionButton',
       dataElement: 'saveDocumentButton',
@@ -94,33 +94,33 @@ function receiveMessage(event) {
   if (event.isTrusted && typeof event.data === 'object') {
     switch (event.data.type) {
       case 'OPEN_DOCUMENT':
-        event.target.readerControl.loadDocument(event.data.file)
+        instance.loadDocument(event.data.file)
         break;
       case 'OPEN_BLOB_OR_LINK':
         let payload = event.data.payload;
         currentDocId = payload.documentId;
-        event.target.readerControl.loadDocument(payload.linkblob, { extension: payload.extension, filename: payload.filename, documentId: payload.documentId })
+        instance.loadDocument(payload.linkblob, { extension: payload.extension, filename: payload.filename, documentId: payload.documentId })
         break;
       case 'OPEN_DOCUMENT_BLOB':
         const { blob, extension, filename, documentId } = event.data.payload;
         console.log("documentId", documentId);
         currentDocId = documentId;
-        event.target.readerControl.loadDocument(blob, { extension, filename, documentId })
+        instance.loadDocument(blob, { extension, filename, documentId })
         break;
       case 'DOCUMENT_SAVED':
-        readerControl.showErrorMessage('Document saved!')
+        instance.showErrorMessage('Document saved!')
         setTimeout(() => {
-          readerControl.closeElements(['errorModal', 'loadingModal'])
+          instance.closeElements(['errorModal', 'loadingModal'])
         }, 2000)
         break;
       case 'LMS_RECEIVED':  
-        event.target.readerControl.loadDocument(event.data.payload.message, {
+          instance.loadDocument(event.data.payload.message, {
           filename: event.data.payload.filename,
           withCredentials: false
         });
         break;
       case 'CLOSE_DOCUMENT':
-        event.target.readerControl.closeDocument()
+        instance.closeDocument()
         break;
       default:
         break;
